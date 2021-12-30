727569ccfaadbb69ff1fa5045f2d7ea853889203
Arz 4cordes <armel.abautret@gmail.com>
2021-12-30 10:57:50 +0100

    test de loadCompetitions avec un fichier vide renvoie une erreur

--- Python_Testing-master/tests/unit_tests/test_server.py
+++ Python_Testing-master/tests/unit_tests/test_server.py
@@ -12,6 +12,12 @@ class MockResponseClubs:
         {"name": "myclub", "email": "abc@mybox.com", "points": "25"}
     ]}
 
+class MockResponseEmptyJsonFile:
+
+    @staticmethod
+    def load(a_file):
+        return {} 
+
 def test_loadClubs_return_listOfClubs(monkeypatch):
 
     def mock_get(*args, **kwargs):
@@ -22,3 +28,31 @@ def test_loadClubs_return_listOfClubs(monkeypatch):
         {"name": "myclub", "email": "abc@mybox.com", "points": "25"}
     ]
     assert loadClubs() == expected_value
+
+class MockResponseCompetitions:
+
+    @staticmethod
+    def load(a_file):
+        return {"competitions":[
+        {"name": "Legends", "date": "2020-10-22 13:30;00", "numberOfPlaces": "25"}
+    ]}
+
+def test_loadCompetitions_return_listOfCompetitions(monkeypatch):
+
+    def mock_get(*args, **kwargs):
+        return MockResponseCompetitions().load('competitions.json')
+
+    monkeypatch.setattr(json, "load", mock_get)
+    expected_value = [
+        {"name": "Legends", "date": "2020-10-22 13:30;00", "numberOfPlaces": "25"}
+    ]
+    assert loadCompetitions() == expected_value
+
+def test_loadCompetitions_with_empty_file(monkeypatch):
+
+    def mock_get(*args, **kwargs):
+        return MockResponseEmptyJsonFile().load('competitions.json')
+
+    monkeypatch.setattr(json, "load", mock_get)
+    expected_value = {}
+    assert loadCompetitions() == expected_value
