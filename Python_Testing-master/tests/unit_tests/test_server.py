371505363160dd9d16fded85cb64a6de838777d5
Arz 4cordes <armel.abautret@gmail.com>
2022-01-04 18:33:45 +0100

    Ajout des tests de la fonction index

--- Python_Testing-master/tests/unit_tests/test_server.py
+++ Python_Testing-master/tests/unit_tests/test_server.py
@@ -1,10 +1,20 @@
 import json
-import os
+
+import pytest
+from flask import request
 
 import server
 from server import loadClubs, loadCompetitions, index, showSummary, \
                    book, purchasePlaces, logout
 
+"""
+Test de la fonction loadClubs:
+Verifier que la fonction renvoie bien une liste de clubs
+indiquée dans un fichier json
+Verifier que la fonction crée une clé Clubs
+et renvoie une liste vide
+si le fichier json ne contient pas la clé Clubs
+"""
 class MockResponseClubs:
 
     @staticmethod
@@ -43,6 +53,14 @@ def test_loadClubs_with_empty_file(monkeypatch):
     expected_value = []
     assert loadClubs() == expected_value
 
+"""
+Test de la fonction loadCompetitions:
+Verifier que la fonction renvoie bien une liste de competitions
+indiquée dans un fichier json
+Verifier que la fonction crée une clé Compétitions
+et renvoie une liste vide si le fichier
+json ne contient pas la clé Competitions
+"""
 class MockResponseCompetitions:
 
     @staticmethod
@@ -80,3 +98,30 @@ def test_loadCompetitions_with_empty_file(monkeypatch):
     monkeypatch.setattr(server, "_initialize_competitions", mockreturn)
     expected_value = []
     assert loadCompetitions() == expected_value
+
+"""
+Test de la fonction index:
+Vérifier que l'accès à la page est OK
+Vérifier que la requête post n'est pas permise
+Vérifier que la requète http fonctionne avec l'url mentionnée
+"""
+@pytest.fixture
+def client():
+    new_app = server.app
+    new_app.testing = True
+    with new_app.test_client() as c:
+        yield c
+
+def test_index_status(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_index_wrong_method(client):
+    response = client.post('/')
+    assert response.status_code == 405
+
+def test_index_context():
+    the_app = server.app
+    the_app.testing = True
+    with the_app.test_request_context('/'):
+        assert request.path == '/'
